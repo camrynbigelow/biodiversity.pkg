9bbc49bec9196e1daf082936fc0e33d06e8571b0
Camryn Bigelow <camryn.bigelow@selu.edu>
2023-11-09 20:43:16 +0000

    switching function names to match dataset

--- R/mapping_abundance.R
+++ R/mapping_abundance.R
@@ -2,37 +2,37 @@
 #' 
 #' Plot abundances in a map. Need the following packages: tidyverse, sf, mapview, and vegan
 #' @param data
-#' @param species.name The species or common name column
-#' @param latitude The column with latitude coordinates
-#' @param longitude The column with longitude coordinates
-#' @param sector From sectors 1-6
+#' @param Species The species or common name column
+#' @param Lat The column with Lat coordinates
+#' @param Long The column with Long coordinates
+#' @param Sector From Sectors 1-6
 #' @return abundanceMap The interactive map of abundances
 #' 
 #' @export
 
-mapping_abundance<-function(data, species.name, latitude, longitude, sector){
+mapping_abundance<-function(data, Species, Lat, Long, Sector){
   speciesCount<-fake %>%
-    select(species.name, count, sector)%>%
-    group_by(species.name, sector) %>% 
+    select(Species, count, Sector)%>%
+    group_by(Species, Sector) %>% 
     summarize(totalCount=sum(count)) 
-  ggplot(data, aes(x=longitude, y=latitude, color = sector))+
+  ggplot(data, aes(x=Long, y=Lat, color = Sector))+
     geom_point()+
     theme_bw()
-  deployCount <- fake %>% dplyr::select(species.name, count, longitude, latitude, sector) %>% 
-    group_by(longitude, latitude, sector) %>% 
+  deployCount <- fake %>% dplyr::select(Species, count, Long, Lat, Sector) %>% 
+    group_by(Long, Lat, Sector) %>% 
     summarize(totalCount = sum(count))
   
   ggplot(deployCount)+
-    geom_point(aes(x=longitude, y=latitiude, size=totalCount, color=sector), alpha=0.5)+
+    geom_point(aes(x=Long, y=latitiude, size=totalCount, color=Sector), alpha=0.5)+
     theme_bw()
   
-  deployCount_sf <- deployCount %>% st_as_sf(coords = c("longitude", "latitude"), crs=4326)
+  deployCount_sf <- deployCount %>% st_as_sf(coords = c("Long", "Lat"), crs=4326)
   
-  abundanceMap <- mapview(deployCount_sf, zcol="sector", cex="totalCount", layer.name = "Sector Name")
+  abundanceMap <- mapview(deployCount_sf, zcol="Sector", cex="totalCount", layer.name = "Sector Name")
   
   return(abundanceMap)
 }
-#mapping_abundance(fake, species.name, latitude, longitude, sector)
+#mapping_abundance(fake, Species, Lat, Long, Sector)
 
-#Need help making sector a character
+#Need help making Sector a character
 
