7bfa3972833056cfa44ccd4c1465981829a2a178
Camryn Bigelow <camryn.bigelow@selu.edu>
2023-11-21 21:30:07 +0000

    shortened fxn to speciesMatrix

--- R/nmds_plot.R
+++ R/nmds_plot.R
@@ -1,10 +1,10 @@
-#'Non-metric Multidimensional Scale plot
+#'Non-metric Multidimensional Scale prep
 #'
-#'Creates a ggplot based on non-metric multidimensional scale to show similarity between groups. 
+#'Prepares data for NMDS plotting.
 #'@param data The data you cleaned (dataframe)
 #'@param Species Your species name column
 #'@param Sector Your sector column
-#'@return plot Your nmds plot using ggplot
+#'@return speciesMatrix 
 #'
 #'@export
 
@@ -19,13 +19,23 @@ nmds_plot<-function(data, Species, Sector){
     summarize(totalCount = sum(Count)) %>%
     spread(Species, totalCount, fill = 0) 
   speciesMatrix <- data.matrix(speciesCount, rownames.force = unique(data$Sector))
-  NMDS <- metaMDS(speciesMatrix)
-  data.scores = as.data.frame(scores(NMDS)$sites)
-  names <- data.scores  %>%
-    mutate(Location  = speciesCount$Sector)
-  plot <- ggplot(names, aes(x = NMDS1, y = NMDS2)) +
-    geom_point(size = 4, aes(colour = Location))
-  return(plot)
+  return(speciesMatrix)
   }
 }
+
+
+    speciesCount <- invert %>% 
+      select(Species, Count, Sector) %>% 
+      group_by(Species, Sector) %>% 
+      summarize(totalCount = sum(Count)) %>%
+      spread(Species, totalCount, fill = 0) 
+    speciesMatrix <- data.matrix(speciesCount, rownames.force = unique(invert$Sector))
+    NMDS <- metaMDS(speciesMatrix)
+    data.scores = as.data.frame(scores(NMDS)$sites)
+    names <- data.scores  %>%
+      mutate(Location  = speciesCount$Sector)
+    plot <- ggplot(names, aes(x = NMDS1, y = NMDS2)) +
+      geom_point(size = 4, aes(colour = Location))
+
+  
 
